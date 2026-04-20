dee14f147b1b168307f800b68e6badfaad69683c
LEEMACHO <54274923+LEEMACHO@users.noreply.github.com>
2026-04-20 18:11:03 +0900

    Add files via upload

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(stamina, speed, accel) {
+    this.stamina = stamina;
+    this.speed = speed;
+    this.accel = accel;
+  }
+}
+
+// 초기 플레이어 (70,60,60)
+const mainPlayer = new Player(70, 60, 60);
