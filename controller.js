9e4872f8f5fa3b8374a16d46b64cb208d6735e1e
LEEMACHO <54274923+LEEMACHO@users.noreply.github.com>
2026-04-20 18:19:14 +0900

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,10 +1,12 @@
 const startButton = document.getElementById("start-btn");
 const resetButton = document.getElementById("reset-btn");
+const countdownDisplay = document.getElementById("countdown");
+
+let countdownInterval;
 
 startButton.addEventListener("click", () => {
-  console.log("경기 시작!");
-  startRace();
-});
-
-resetButton.addEventListener("click", () => {
-  console.log("
+  let count = 3;
+  countdownDisplay.textContent = count;
+  countdownInterval = setInterval(() => {
+    count--;
+    if (
