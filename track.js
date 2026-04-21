8dea16478cbdf0c4851d1d7aa268d262b088617d
LEEMACHO <54274923+LEEMACHO@users.noreply.github.com>
2026-04-21 16:42:58 +0900

    Update track.js
    1000px 변경

--- track.js
+++ track.js
@@ -1,8 +1,9 @@
 class Track {
   constructor(lengthPx, lengthM) {
-    this.lengthPx = lengthPx;
-    this.lengthM = lengthM;
+    this.lengthPx = lengthPx; // 실제 달려야 할 거리 (1000px)
+    this.lengthM = lengthM;   // 현실 미터법 기준 (100m)
   }
 }
 
-const track = new Track(720, 100);
+// 주행 거리를 1000px로 설정
+const track = new Track(1000, 100);
